3fa0aa73e664149df075c99530b4ad52501a2a5d
vivek <vivek@creatingwinners.com>
2022-06-22 03:01:19 +0800

    var and val

--- kotlinlab/src/main/kotlin/Main.kt
+++ kotlinlab/src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
+val PI = 3.14
+var x = 1;
+
 fun main(args: Array<String>) {
-    println(sum(4, 3))
-    printSum(3,4);
+//    println(sum(4, 3))
+//    printSum(3,4);
+
+
+    val a: Int = 1  // immediate assignment
+    val b = 2   // `Int` type is inferred
+    val c: Int  // Type required when no initializer is provided
+    c = 3       // deferred assignment
+    println("$a : $b : $c")
+
+    //x incremented
+    println(x)
+    increment()
+    println(x)
+
 }
+
+fun increment() = x++
 
 //A function body can be an expression. Its return type is inferred./auto judjed
 //fun sum(a: Int, b: Int): Int {
