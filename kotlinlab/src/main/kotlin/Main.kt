6f69d9392a23ebe832a211d0c54866c152a85ee0
vivek <vivek@creatingwinners.com>
2022-06-22 02:53:45 +0800

    function return type inferred & can be use with Unit

--- kotlinlab/src/main/kotlin/Main.kt
+++ kotlinlab/src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println(sum(4, 3))
+    printSum(3,4);
+}
+
+//A function body can be an expression. Its return type is inferred./auto judjed
+//fun sum(a: Int, b: Int): Int {
+//    return a + b
+//}
+fun sum(a: Int, b: Int) = a + b
+
+//Unit can be omitted
+fun printSum(a: Int, b: Int) : Unit {
+    println("sum of $a and $b is ${a+b}")
+    println("sum of $a and $b is ${a + b}")
 }
