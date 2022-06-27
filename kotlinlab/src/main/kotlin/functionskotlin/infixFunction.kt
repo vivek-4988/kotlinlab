eca827d820e98bf908ef19e7beacecb3def028ab
vivek <vivek@creatingwinners.com>
2022-06-28 01:34:57 +0800

    infixFunction

--- kotlinlab/src/main/kotlin/functionskotlin/infixFunction.kt
+++ kotlinlab/src/main/kotlin/functionskotlin/infixFunction.kt
@@ -2,7 +2,17 @@ package functionskotlin
 
 fun main() {
     println(1.doPlus(2))
+    println(2 times "Bye")
+
+    val pair = "Ferrari" to "Katrina"                          // 3
+    println(pair)
+    infix fun String.onto(other: String) = Pair(this, other)   // 4
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
 }
 
 //infix allows only one parameter
 infix fun Int.doPlus(a: Int) = a*a
+
+
+infix fun Int.times(str: String) = str.repeat(this)        // 1
