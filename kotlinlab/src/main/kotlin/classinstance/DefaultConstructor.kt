d55c1cbcc1ad495d9351ed1980bcfe4acc1d0441
vivek <vivek@creatingwinners.com>
2022-06-27 12:36:35 +0800

    init

--- kotlinlab/src/main/kotlin/classinstance/DefaultConstructor.kt
+++ kotlinlab/src/main/kotlin/classinstance/DefaultConstructor.kt
@@ -3,4 +3,5 @@ import classinstance.Rectangle
 fun main(args: Array<String>) {
     val rectangle = Rectangle(5.0,8.0)
     println("${rectangle.param}")
+    println("${rectangle.greetings}")
 }
--- kotlinlab/src/main/kotlin/classinstance/Rectangle.kt
+++ kotlinlab/src/main/kotlin/classinstance/Rectangle.kt
@@ -3,4 +3,11 @@ package classinstance
 //Properties of a class can be listed in its declaration or body.
 class Rectangle(var height:Double,var length:Double) {
      var param = (height + length) * 2;
+     val greetings : String
+
+     //todo Initializer is the common parts to initialize the instance but not depended on arguments passing.
+     init {
+          param += 5;
+          greetings  = "welcome"
+     }
 }
