384ade8fe019853b95a5fa5f525a74877fd90c0b
vivek <vivek@creatingwinners.com>
2022-06-29 11:55:22 +0800

    == and ===

--- kotlinlab/src/main/kotlin/conditional/if_expressions.kt
+++ kotlinlab/src/main/kotlin/conditional/if_expressions.kt
@@ -2,6 +2,14 @@ package conditional
 
 fun main(){
     print(maxOfSimple(4,5))
+
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+    val oddWriters = setOf("Shakespeare", "Hemingway", "Twain")
+
+    println(authors == writers)
+    println(authors === writers)//only true if its in order
+    println(authors == oddWriters)
 }
 
 //todo if conditions
