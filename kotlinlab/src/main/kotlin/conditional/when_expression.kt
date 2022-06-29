e27c995c6a370607e5e3eee66e181514c0bc5cd5
vivek <vivek@creatingwinners.com>
2022-06-29 11:37:14 +0800

    when as expression

--- kotlinlab/src/main/kotlin/conditional/when_expression.kt
+++ kotlinlab/src/main/kotlin/conditional/when_expression.kt
@@ -2,6 +2,7 @@ package conditional
 
 fun main(){
     println(describe(131232))
+    println(whenAssign(1))
 }
 
 fun describe(obj:Any): String =
@@ -12,3 +13,13 @@ fun describe(obj:Any): String =
         !is String -> "Not a string"
         else -> "Else"
     }
+
+fun whenAssign(obj: Any): Any {
+    val result = when (obj) {                   // 1
+        1 -> "one"                              // 2
+        "Hello" -> 1                            // 3
+        is Long -> false                        // 4
+        else -> 42                              // 5
+    }
+    return result
+}
