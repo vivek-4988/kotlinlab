5ec75265ac2555246e40bac2b514f093566c07b7
vivek <vivek@creatingwinners.com>
2022-07-04 16:12:53 +0800

    extension function update

--- kotlinlab/src/main/kotlin/functionskotlin/funExtension.kt
+++ kotlinlab/src/main/kotlin/functionskotlin/funExtension.kt
@@ -2,16 +2,29 @@ package functionskotlin
 
 import kotlinspecial.forSingleTone
 
-fun main(){
+
+//Namely, there are two types of extensions: extension functions and extension properties.
+// They look a lot like normal functions and properties but with one important difference:
+// you need to specify the type that you extend.
+data class Item(val name: String, val price: Float)                                         // 1
+
+data class Order(val items: Collection<Item>)
+
+fun Order.maxPricedItemValue(): Float = this.items.maxByOrNull { it.price }?.price ?: 0F    // 2
+fun Order.maxPricedItemName() = this.items.maxByOrNull { it.price }?.name ?: "NO_PRODUCTS"
+
+fun main() {
     val myString = "Hello Earth"
     val getSingleTpne = forSingleTone.name
     println(myString.makeCamel())
     println(myString.removeFirstLastChar())
 
     println(getSingleTpne)
+
+    val order = Order(listOf(Item("Bread", 25.0F), Item("Wine", 29.0F), Item("Water", 12.0F)))
+    println("Max priced item name: ${order.maxPricedItemName()}")                           // 4
+    println("Max priced item value: ${order.maxPricedItemValue()}")
 }
 
 fun String.makeCamel(): String = uppercase()
-
-
-fun String.removeFirstLastChar(): String =  this.substring(1, this.length - 1)
+fun String.removeFirstLastChar(): String = this.substring(1, this.length - 1)
