802a380b705aaf110a623cd347e43a6e576c60f0
vivek <vivek@creatingwinners.com>
2022-07-05 22:04:20 +0800

    minOrnull and maxOrNull

--- kotlinlab/src/main/kotlin/collections/listcollection.kt
+++ kotlinlab/src/main/kotlin/collections/listcollection.kt
@@ -13,6 +13,17 @@ fun main(){
     getSysUser().forEach {
         i-> print("$i ,")
     }
+
+
+    println("========= minOrNull and maxOrNull ============")
+    val numbers = listOf(1, 2, 3)
+    val empty = emptyList<Int>()
+    val only = listOf(3)
+
+    println("Numbers: $numbers, min = ${numbers.minOrNull()} max = ${numbers.maxOrNull()}") // 1
+    println("Empty: $empty, min = ${empty.minOrNull()}, max = ${empty.maxOrNull()}")        // 2
+    println("Only: $only, min = ${only.minOrNull()}, max = ${only.maxOrNull()}")
+
 }
 
 fun addSystemUser(newUser:Int){
